ad1b9a45ca92632b27eebf63cad704a78d9124e0
Priyanka Prakash Sahu <priyankasahu@adobe.com>
2025-07-28 06:01:03 +0530

    alt text deleted

---
+++ blocks/banner-carousel/banner-carousel.js
@@ -0,0 +1,16 @@
+export default function decorate(block)
+{
+    const carousel = document.createElement('div');
+    carousel.className= "carousel";
+
+    const carouselItem = document.createElement('div');
+    carouselItem.className = "carousel-item";
+
+    const desktopBanner = document.createElement('div');
+    carouselItem.className = "desktop-banner";
+
+    const mobileBanner = document.createElement('div');
+    carouselItem.className = "desktop-banner";
+
+    
+}
